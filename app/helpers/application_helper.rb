f86f253272c38019d9d0b67bd60b767fbb7dc402
Richard Hernandez <rhsequence@gmail.com>
2011-12-03 22:16:59 -0600

    Add CSS sheets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	
+	#Return a title on a per-page basis.
+	def title
+		basetitle = "Ruby on Rails Tutorial Sample App"
+		if @title.nil?
+		   basetitle
+	  	else
+		   "#{basetitle} | #{@title}"
+		end
+	end
 end
